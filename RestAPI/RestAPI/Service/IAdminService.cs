6df9957c7b7ed50fe798770528c5a7f290f43e66
DozsaRoland <dozsa.roland@njit.hu>
2021-12-15 15:17:04 +0100

    Service 1

---
+++ RestAPI/RestAPI/Service/IAdminService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPI.Service
+{
+   public interface IAdminService : IService<Admin>
+    {
+        List<IAdminService> GetUserbyAdmin(int adminId);
+    }
+}
---
+++ RestAPI/RestAPI/Service/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using RestAPI.Entity;
+
+namespace RestAPI.Service
+{
+    public interface IService<T> where T : EntityBase
+    {
+        T GetById(int id);
+        List<T> GetAll();
+        T Save(T inputData);
+        T Update(int id, T updateData);
+        void Delete(int id);
+    }
+}
