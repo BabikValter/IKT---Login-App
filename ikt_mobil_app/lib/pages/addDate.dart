6173770055d8fb75435a292b102bab381db67737
BabikValter <babik.valter@njit.hu>
2022-01-24 11:22:28 +0100

    Routes

---
+++ ikt_mobil_app/lib/pages/addDate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddDate extends StatefulWidget {
+  const AddDate({Key? key}) : super(key: key);
+
+  @override
+  _AddDateState createState() => _AddDateState();
+}
+
+class _AddDateState extends State<AddDate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ ikt_mobil_app/lib/pages/dates.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dates extends StatefulWidget {
+  const Dates({Key? key}) : super(key: key);
+
+  @override
+  _DatesState createState() => _DatesState();
+}
+
+class _DatesState extends State<Dates> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
