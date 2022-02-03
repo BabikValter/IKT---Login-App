0b3a81702145e699c13e871ba61d7e1a7a66ac95
BabikValter <babik.valter@njit.hu>
2022-02-03 19:08:42 +0100

    listing beta

--- ikt_loginapp_final/lib/screens/home/home.dart
+++ ikt_loginapp_final/lib/screens/home/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ikt_loginapp_final/models/brew.dart';
 import 'package:ikt_loginapp_final/services/auth.dart';
+import 'package:ikt_loginapp_final/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   Home({ Key? key }) : super(key: key);
@@ -8,21 +11,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: const Text('Időpontfoglalás'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: <Widget>[
-          TextButton.icon(
-            icon: const Icon(Icons.person),
-            label: const Text('Kijelentkezés'),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      initialData: const [],
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: const Text('Időpontfoglalás'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            TextButton.icon(
+              icon: const Icon(Icons.person),
+              label: const Text('Kijelentkezés'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
--- ikt_loginapp_final/lib/services/auth.dart
+++ ikt_loginapp_final/lib/services/auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:ikt_loginapp_final/models/auser.dart';
+import 'package:ikt_loginapp_final/services/database.dart';
 
 class AuthService {
 
@@ -47,6 +48,9 @@ class AuthService {
     try{
       UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
       User? user = result.user;
+
+      // create document for user with the uid
+      await DatabaseService(uid: user!.uid).updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch(e) {
       debugPrint(e.toString());
